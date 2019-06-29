3db00213c4ee181157b66760420a4aee384bdf05
megangilpin <48898520+megangilpin@users.noreply.github.com>
2019-06-28 23:38:16 -0400

    added spotify api call

--- liri.js
+++ liri.js
@@ -1,35 +1,50 @@
 // add code to read and set any environment variables with the dotenv package:
 require("dotenv").config();
+
+// Require methods for json package files
 var inquirer = require("inquirer");
 var axios = require("axios");
 var moment = require("moment");
+var Spotify = require('node-spotify-api');
 
 // code required to import the keys.js file
-// var keys = require("keys.js");
-
-// require for Spotify node package
-// var Spotify = require('node-spotify-api');
+var keys = require("./keys.js");
 
 // access keys information
-// var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 
-// Spotify search function
-// spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
-//   if (err) {
-//     return console.log('Error occurred: ' + err);
-//   }
 
-//   console.log(data);
-// });
+var showSong = function () {
 
-// spotify
-//   .search({ type: 'track', query: 'All the Small Things' })
-//   .then(function (response) {
-//     console.log(response);
-//   })
-//   .catch(function (err) {
-//     console.log(err);
-//   });
+  inquirer.prompt([
+    {
+      name: "song",
+      message: "What song do you want to find?"
+    },
+  ]).then(function (answers) {
+  spotify
+    .search({ type: 'track', query: answers.song, limit:5})
+    .then(function (response) {
+
+      var songs = response.tracks.items
+      
+      for (var i = 0; i < songs.length; i++){
+        var artist = songs[i].artists[0].name;
+        var songTitle = songs[i].name;
+        var albumName = songs[i].album.name;
+        var songPreview = songs[i].preview_url;
+  
+        console.log("--------------" + "\nArtist: " + artist + "\nSong Title: " + songTitle + "\nSong Preview: " + songPreview + "\nAlbum Name: " + albumName + "\n--------------")
+      }
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+  })
+}
+
+showSong()
+
 
 // Uses inquirer to prompt user for artist they want to see then uses axios to call Bands In Town API
 var showConcert = function () {
@@ -49,7 +64,6 @@ var showConcert = function () {
     
     axios.get(queryUrlBandsintown).then(
       function (response) {
-          // console.log(response);
           for (var i = 0; i < 5; i++){
             console.log("--------------" + "\nVenue Name: " + response.data[i].venue.name + "\nVenue Location: " + response.data[i].venue.city + "\nEvent Date: " + moment(response.data[i].datetime).format("MM/DD/YYYY") + "\n--------------");
           }
@@ -59,4 +73,4 @@ var showConcert = function () {
   )
 }
 
-showConcert()
+// showConcert()
